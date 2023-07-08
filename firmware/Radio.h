bbc26a67330d274a0ccbd0a9c6ea4eb4dd73c6f5
jake <jaketeater82@gmail.com>
2023-07-08 14:56:54 -0400

    refactored code into Radio.h

--- firmware/Radio.h
+++ firmware/Radio.h
@@ -12,38 +12,50 @@ It is unaware of the Audio & LEDStatus objects.
 #include "Audio.h"
 #include "LEDStatus.h"
 
-
-#define RADIO_DEFAULT_REMOTE_LIST_URL ""
-#define RADIO_DEFAULT_REMOTE_LIST false
-#define RADIO_DEFAULT_HAS_CHANNEL_POT false
-#define RADIO_DEFAULT_PCB_VERSION ""
-#define RADIO_DEFAULT_RADIO_ID ""
-
-#define RADIO_DEFAULT_STATION_ONE_URL ""
-#define RADIO_DEFAULT_STATION_TWO_URL ""
-#define RADIO_DEFAULT_STATION_THREE_URL ""
-#define RADIO_DEFAULT_STATION_FOUR_URL ""
-#define RADIO_DEFAULT_STATION_COUNT 1
-
-#define RADIO_DEFAULT_MAX_STATION_COUNT 4  // This is determined by hardware
-
-#define RADIO_DEBUG_STATUS_UPDATE_INTERVAL_MS 5000
-
-#define RADIO_VOLUME_CHANNEL_READ_INTERVAL_MS 50
-
-#define RADIO_CURRENT_TIME_CHECK_INTERVAL_S 3
-
-
+// Move these to the radio config?
+// #define RADIO_CURRENT_TIME_CHECK_INTERVAL_S 3
 
 WiFiClient client;
 HTTPClient http;
 
+struct RadioConfig {
+
+  // Hardware
+  int pinChannelPot = 2;
+  int pinVolumePot = 8;
+  int pinDacSdMode = 11;
+  int pinI2sDout = 12;
+  int pinI2sBclk = 13;
+  int pinI2sLrc = 14;
+  bool hasChannelPot = true;
+  String pcbVersion = "";
+  int analogReadResolution = 12;  // This ensures the maps used in readChannelIdx and readVolume are correct.
+
+  // Software
+  int volumeMin = 0;
+  int volumeMax = 21;
+  String stn1URL = "";
+  String stn2URL = "";
+  String stn3URL = "";
+  String stn4URL = "";
+  int stationCount = 1;
+  int maxStationCount = 4;
+
+  // Intervals
+  int debugStatusUpdateIntervalMs = 5000;
+  int analogReadIntervalMs = 50;
+  int streamLossDetectionIntervalS = 3;
+
+  // Remote Config
+  bool remoteConfig = false;
+  String remoteConfigURL = "";
+  String radioID = "";
+};
+
 struct RadioStatus {
 
   // This will be replaced by the below
-  // int channelIdx;
   bool playing;
-
 
   // These statuses are parsed to set the status code, which is used to set the LEDs
 
@@ -59,10 +71,11 @@ struct RadioStatus {
   int volume = 0;
 
   // Statuses (Associated with actions)
-  bool wifiConnected = false;      // Satus of WiFi after booting. (The initial connection is handled by WifiManager, and the LEDs should be set there.)
-  bool streamConnecting = false;   // This is set before connecting to a new stream (whether starting or when changing stations)
-  bool streamPlaying = false;      // (this is volume > 0 and connection successful) Changing volume from 0 to something greater than 0 requests the stream. Changing the volume to 0 stops the stream.
-  bool streamIsAdvancing = false;  // This is set by checking if the number of seconds played has advanced
+  bool wifiConnected = false;       // Satus of WiFi after booting. (The initial connection is handled by WifiManager, and the LEDs should be set there.)
+  bool streamConnecting = false;    // This is set before connecting to a new stream (whether starting or when changing stations)
+  bool streamReConnecting = false;  // Connection after a lost connection
+  bool streamPlaying = false;       // (this is volume > 0 and connection successful) Changing volume from 0 to something greater than 0 requests the stream. Changing the volume to 0 stops the stream.
+  bool streamIsAdvancing = false;   // This is set by checking if the number of seconds played has advanced
 
   // These are set during blocking and should be reported as errors, not statuses - ex: initialConnectionSuccessful is by default false until it's set to true - but it's not an error until the connection times out
   // bool streamInitialConnectionSuccessful = false;  // Starting the stream returns success/failure, which goes here.
@@ -93,17 +106,13 @@ Clear error code????
 };
 
 class Radio {
-  int mChannelPotPin;
-  int mVolumePotPin;
-  int mVolumeMin;
-  int mVolumeMax;
   unsigned long lastVolumeChannelRead = 0;
   unsigned long lastCurrentTimeCheck = 0;
   uint32_t lastCurrentTime = 0;
 
   /*
 
-    Split the fader up into 13 parts (0-12), which correpsond with 3 channels.
+    Split the fader up into 13 parts (0-12), which correspond with 3 channels.
 
     0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12
     |    3    |              2            |      1      |
@@ -129,46 +138,33 @@ class Radio {
   unsigned long lastMinuteInterval = 0;
 
 public:
-  Radio(int channelPotPin, int volumePotPin, int volumeMin, int volumeMax, WiFiManager *myWifiManager, Audio *myAudio);
+  Radio(RadioConfig *config, WiFiManager *myWifiManager, Audio *myAudio);
   void init();
   void getConfigFromPreferences();
   void putConfigToPreferences();
   int readChannelIdx();
   int readVolume();
-  void setDebugMode();
+  void initDebugMode();
   bool getConfigFromRemote();
   void debugPrintConfigToSerial();
   void setStatusCode();
   void loop();
   void debugModeLoop();
+  void setDacSdMode(bool enable);
   bool debugMode = false;
-  RadioStatus status;
   Preferences preferences;
   LEDStatus ledStatus;
+  RadioStatus status;
+  RadioConfig *config;
   WiFiManager *wifiManager;
   Audio *audio;
-
-  // Config
-  String remoteConfigURL = RADIO_DEFAULT_REMOTE_LIST_URL;
-  bool remoteConfig = RADIO_DEFAULT_REMOTE_LIST;
-  String radioID = RADIO_DEFAULT_RADIO_ID;
-  bool hasChannelPot = RADIO_DEFAULT_HAS_CHANNEL_POT;
-  String pcbVersion = RADIO_DEFAULT_PCB_VERSION;
-  String stn1URL = RADIO_DEFAULT_STATION_ONE_URL;
-  String stn2URL = RADIO_DEFAULT_STATION_TWO_URL;
-  String stn3URL = RADIO_DEFAULT_STATION_THREE_URL;
-  String stn4URL = RADIO_DEFAULT_STATION_FOUR_URL;
-  int stationCount = RADIO_DEFAULT_STATION_COUNT;
-  int maxStationCount = RADIO_DEFAULT_MAX_STATION_COUNT;
 };
 
-Radio::Radio(int channelPotPin, int volumePotPin, int volumeMin, int volumeMax, WiFiManager *myWifiManager, Audio *myAudio) {
-  mChannelPotPin = channelPotPin;
-  mVolumePotPin = volumePotPin;
-  mVolumeMin = volumeMin;
-  mVolumeMax = volumeMax;
+Radio::Radio(RadioConfig *myConfig, WiFiManager *myWifiManager, Audio *myAudio) {
+  config = myConfig;
   wifiManager = myWifiManager;
   audio = myAudio;
+
   status.playing = false;
   status.channelIdx = 0;
 }
@@ -176,53 +172,63 @@ Radio::Radio(int channelPotPin, int volumePotPin, int volumeMin, int volumeMax, 
 void Radio::getConfigFromPreferences() {
   // IMPORTANT the preferences library accepts keys up to 15 characters. Larger keys can be passed and no error will be thrown, but strange things may happen.
   preferences.begin("config", false);
-  remoteConfigURL = preferences.getString("remoteConfigURL", RADIO_DEFAULT_REMOTE_LIST_URL);
-  remoteConfig = preferences.getBool("remoteConfig", RADIO_DEFAULT_REMOTE_LIST);
-  radioID = preferences.getString("radioID", RADIO_DEFAULT_RADIO_ID);
-  hasChannelPot = preferences.getBool("hasChannelPot", RADIO_DEFAULT_HAS_CHANNEL_POT);
-  pcbVersion = preferences.getString("pcbVersion", RADIO_DEFAULT_PCB_VERSION);
-  stn1URL = preferences.getString("stn1URL", RADIO_DEFAULT_STATION_ONE_URL);
-  stn2URL = preferences.getString("stn2URL", RADIO_DEFAULT_STATION_TWO_URL);
-  stn3URL = preferences.getString("stn3URL", RADIO_DEFAULT_STATION_THREE_URL);
-  stn4URL = preferences.getString("stn4URL", RADIO_DEFAULT_STATION_FOUR_URL);
-  stationCount = preferences.getInt("stationCount", RADIO_DEFAULT_STATION_COUNT);
+  config->remoteConfigURL = preferences.getString("remoteConfigURL", config->remoteConfigURL);
+  config->remoteConfig = preferences.getBool("remoteConfig", config->remoteConfig);
+  config->radioID = preferences.getString("radioID", config->radioID);
+  config->hasChannelPot = preferences.getBool("hasChannelPot", config->hasChannelPot);
+  config->pcbVersion = preferences.getString("pcbVersion", config->pcbVersion);
+  config->stn1URL = preferences.getString("stn1URL", config->stn1URL);
+  config->stn2URL = preferences.getString("stn2URL", config->stn2URL);
+  config->stn3URL = preferences.getString("stn3URL", config->stn3URL);
+  config->stn4URL = preferences.getString("stn4URL", config->stn4URL);
+  config->stationCount = preferences.getInt("stationCount", config->stationCount);
   preferences.end();
 }
 
 void Radio::putConfigToPreferences() {
   preferences.begin("config", false);
-  preferences.putString("remoteConfigURL", remoteConfigURL);
-  preferences.putBool("remoteConfig", remoteConfig);
-  preferences.putString("radioID", radioID);
-  preferences.putBool("hasChannelPot", hasChannelPot);
-  preferences.putString("pcbVersion", pcbVersion);
-  preferences.putString("stn1URL", stn1URL);
-  preferences.putString("stn2URL", stn2URL);
-  preferences.putString("stn3URL", stn3URL);
-  preferences.putString("stn4URL", stn4URL);
-  preferences.putInt("stationCount", stationCount);
+  preferences.putString("remoteConfigURL", config->remoteConfigURL);
+  preferences.putBool("remoteConfig", config->remoteConfig);
+  preferences.putString("radioID", config->radioID);
+  preferences.putBool("hasChannelPot", config->hasChannelPot);
+  preferences.putString("pcbVersion", config->pcbVersion);
+  preferences.putString("stn1URL", config->stn1URL);
+  preferences.putString("stn2URL", config->stn2URL);
+  preferences.putString("stn3URL", config->stn3URL);
+  preferences.putString("stn4URL", config->stn4URL);
+  preferences.putInt("stationCount", config->stationCount);
   preferences.end();
 }
 
 int Radio::readChannelIdx() {
-  int channelInput = analogRead(mChannelPotPin);
+  int channelInput = analogRead(config->pinChannelPot);
   channelInput = map(channelInput, 0, 4095, 0, 11);
-  return mChannelsLookupTable[stationCount - 1][channelInput];
+  return mChannelsLookupTable[config->stationCount - 1][channelInput];
 }
 
 int Radio::readVolume() {
-  int volumeRaw = analogRead(mVolumePotPin);
-  int volume = map(volumeRaw, 0, 4095, mVolumeMin, mVolumeMax);
+  int volumeRaw = analogRead(config->pinVolumePot);
+  int volume = map(volumeRaw, 0, 4095, config->volumeMin, config->volumeMax);
   return volume;
+}
+
+void Radio::setDacSdMode(bool enable) {
+  // Sets the SD mode pin that's connected to the DAC. true turns the DAC on, false turns the dac off.
+  if (enable) {
+    digitalWrite(config->pinDacSdMode, HIGH);
+  } else {
+    digitalWrite(config->pinDacSdMode, LOW);
+  }
 }
 
 void Radio::init() {
 
-  // This makes sure the maps used in readChannelIdx and readVolume are correct.
-  analogReadResolution(12);
+  analogReadResolution(config->analogReadResolution);
+
+  pinMode(config->pinDacSdMode, OUTPUT);
 
   getConfigFromPreferences();
-  setDebugMode();
+  initDebugMode();
 
   ledStatus.init();
 
@@ -237,14 +243,43 @@ void Radio::init() {
 
   ledStatus.setStatus(LED_STATUS_FADE_SUCCESS_TO_INFO);
   ledStatus.setStatus(LED_STATUS_INFO);
+
+  // Initialize WiFi/WiFiManager
+  WiFi.mode(WIFI_STA);
+  wifiManager->setDebugOutput(debugMode);
+  bool res;
+  res = wifiManager->autoConnect("Radio Setup");
+  if (!res) {
+    if (debugMode) Serial.println("Failed to connect or hit timeout");
+    // This is a hard stop
+    ledStatus.setStatus(LED_STATUS_ERROR_BLINKING);
+    delay(5000);
+    ESP.restart();
+  }
+
+  // Initialize Audio
+  audio->setPinout(config->pinI2sBclk, config->pinI2sLrc, config->pinI2sDout);
+  audio->setVolume(0);
+
+  // Get config from remote server
+
+  ledStatus.setStatus(LED_STATUS_LIGHTS_OFF);
+
+  // Get stations from config server
+  bool error = getConfigFromRemote();
+  if (error) {
+    ledStatus.setStatus(LED_STATUS_WARNING_BLINKING);
+    delay(5000);
+  }
+
 };
 
-void Radio::setDebugMode() {
+void Radio::initDebugMode() {
   int volume = readVolume();
-  if (volume == mVolumeMax) {
+  if (volume == config->volumeMax) {
     delay(3000);
     volume = readVolume();
-    if (volume == mVolumeMin) {
+    if (volume == config->volumeMin) {
       debugMode = true;
       return;
     };
@@ -254,17 +289,17 @@ void Radio::setDebugMode() {
 
 bool Radio::getConfigFromRemote() {
   // returns error: true|false
-  if (!remoteConfig) {
+  if (!config->remoteConfig) {
     return false;
   }
 
   char hasPot[5];
-  (hasChannelPot) ? strcpy(hasPot, "true") : strcpy(hasPot, "false");
-  String url = remoteConfigURL
-               + radioID
-               + "?pcb_version=" + pcbVersion
+  (config->hasChannelPot) ? strcpy(hasPot, "true") : strcpy(hasPot, "false");
+  String url = config->remoteConfigURL
+               + config->radioID
+               + "?pcb_version=" + config->pcbVersion
                + "&firmware_version=" + FIRMWARE_VERSION
-               + "&max_station_count=" + (String)maxStationCount
+               + "&max_station_count=" + (String)config->maxStationCount
                + "&has_channel_potentiometer=" + hasPot;
 
   if (debugMode) {
@@ -290,11 +325,11 @@ bool Radio::getConfigFromRemote() {
     return true;
   }
 
-  stn1URL = doc["stn1URL"].as<String>();
-  stn2URL = doc["stn2URL"].as<String>();
-  stn3URL = doc["stn3URL"].as<String>();
-  stn4URL = doc["stn4URL"].as<String>();
-  stationCount = doc["stationCount"];
+  config->stn1URL = doc["stn1URL"].as<String>();
+  config->stn2URL = doc["stn2URL"].as<String>();
+  config->stn3URL = doc["stn3URL"].as<String>();
+  config->stn4URL = doc["stn4URL"].as<String>();
+  config->stationCount = doc["stationCount"];
   putConfigToPreferences();
 
   if (debugMode) {
@@ -314,33 +349,33 @@ void Radio::debugPrintConfigToSerial() {
   Serial.println("-----------");
   Serial.println("Radio Config");
   Serial.print("remoteConfigURL=");
-  Serial.println(remoteConfigURL);
-  Serial.printf("remoteConfig=%d\n", remoteConfig);
-  Serial.printf("radioID=%s\n", radioID);
-  Serial.printf("hasChannelPot=%d\n", hasChannelPot);
+  Serial.println(config->remoteConfigURL);
+  Serial.printf("remoteConfig=%d\n", config->remoteConfig);
+  Serial.printf("radioID=%s\n", config->radioID);
+  Serial.printf("hasChannelPot=%d\n", config->hasChannelPot);
   Serial.print("stn1URL=");
-  Serial.println(stn1URL);
+  Serial.println(config->stn1URL);
   Serial.print("stn2URL=");
-  Serial.println(stn2URL);
+  Serial.println(config->stn2URL);
   Serial.print("stn3URL=");
-  Serial.println(stn3URL);
+  Serial.println(config->stn3URL);
   Serial.print("stn4URL=");
-  Serial.println(stn4URL);
-  Serial.printf("stationCount=%d\n", stationCount);
-  Serial.printf("maxStationCount=%d\n", maxStationCount);
+  Serial.println(config->stn4URL);
+  Serial.printf("stationCount=%d\n", config->stationCount);
+  Serial.printf("maxStationCount=%d\n", config->maxStationCount);
 }
 
 void Radio::debugModeLoop() {
   debugLPS++;
   debugHandleSerialInput();
-  if (millis() - lastDebugStatusUpdate > RADIO_DEBUG_STATUS_UPDATE_INTERVAL_MS) {
+  if (millis() - lastDebugStatusUpdate > config->debugStatusUpdateIntervalMs) {
     Serial.printf("radio.readVolume()=%d\n", readVolume());
     Serial.print("radio.readChannelIdx()=");
     Serial.println(readChannelIdx());
     Serial.printf("radio.status.channelIdx=%d\n", status.channelIdx);
     Serial.printf("radio.status.playing=%d\n", status.playing);
 
-    int lps = debugLPS / (RADIO_DEBUG_STATUS_UPDATE_INTERVAL_MS / 1000);
+    int lps = debugLPS / (config->debugStatusUpdateIntervalMs / 1000);
     debugLPS = 0;
     Serial.printf("lps=%d\n", lps);
 
@@ -357,24 +392,21 @@ void Radio::debugHandleSerialInput() {
       serializeJson(doc, Serial);
       Serial.println("");
 
-      remoteConfigURL = doc["remoteConfigURL"] | remoteConfigURL;
-      remoteConfig = doc["remoteConfig"] | remoteConfig;
-      radioID = doc["radioID"] | radioID;
+      config->remoteConfigURL = doc["remoteConfigURL"] | config->remoteConfigURL;
+      config->remoteConfig = doc["remoteConfig"] | config->remoteConfig;
+      config->radioID = doc["radioID"] | config->radioID;
 
       // should the stuff after | be there? if it works, then leave it alone
-      hasChannelPot = doc["hasChannelPot"] | hasChannelPot;
-      pcbVersion = doc["pcbVersion"] | pcbVersion;
-      stn1URL = doc["stn1URL"] | stn1URL;
-      stn2URL = doc["stn2URL"] | stn2URL;
-      stn3URL = doc["stn3URL"] | stn3URL;
-      stn4URL = doc["stn4URL"] | stn4URL;
+      config->hasChannelPot = doc["hasChannelPot"] | config->hasChannelPot;
+      config->pcbVersion = doc["pcbVersion"] | config->pcbVersion;
+      config->stn1URL = doc["stn1URL"] | config->stn1URL;
+      config->stn2URL = doc["stn2URL"] | config->stn2URL;
+      config->stn3URL = doc["stn3URL"] | config->stn3URL;
+      config->stn4URL = doc["stn4URL"] | config->stn4URL;
 
       if (doc["stationCount"]) {
-        stationCount = doc["stationCount"];
+        config->stationCount = doc["stationCount"];
       }
-
-      /* This is currently limited by the lookup table in the ChannelPot object */
-      // config.maxStationCount = doc["maxStationCount"];
 
       putConfigToPreferences();
 
@@ -413,7 +445,9 @@ void Radio::checkWiFiDisconnect() {
 }
 
 void Radio::loop() {
-  // copy and paste everything over here and get it to compile again, lol
+
+  audio->loop();
+
   if (debugMode) {
     debugModeLoop();
   }
@@ -421,7 +455,7 @@ void Radio::loop() {
   // Check if the WiFi has disconnected
   checkWiFiDisconnect();
 
-  if (millis() > lastVolumeChannelRead + RADIO_VOLUME_CHANNEL_READ_INTERVAL_MS) {
+  if (millis() > lastVolumeChannelRead + config->analogReadIntervalMs) {
 
     // Read the current state of the radio's inputs
     int selectedVolume = readVolume();
@@ -444,10 +478,13 @@ void Radio::loop() {
 
     if (selectedVolume > 0 && !status.playing) {
 
+      setDacSdMode(true);  // Turn DAC on
+
       // start play, set status.playing
       ledStatus.setStatus(LED_STATUS_INFO_BLINKING);
 
-      String channels[] = { stn1URL, stn2URL, stn3URL, stn4URL };
+      // This should be refactored, seems excessive? Can pointers be used here?
+      String channels[] = { config->stn1URL, config->stn2URL, config->stn3URL, config->stn4URL };
       char selectedChannelURL[2048];
       channels[status.channelIdx].toCharArray(selectedChannelURL, 2048);
       status.playing = audio->connecttohost(selectedChannelURL);
@@ -467,19 +504,19 @@ void Radio::loop() {
 
     if (selectedVolume == 0 && status.playing) {
       // stop play, set status
+      setDacSdMode(false);  // Turn DAC off
       status.playing = false;
       audio->stopSong();
       ledStatus.setStatus(LED_STATUS_LIGHTS_OFF);
     }
 
     // Check if the audio->getAudioCurrentTime() is advancing, if not, set the status to match (triggering a reconnect).
-    // TODO: RADIO_CURRENT_TIME_CHECK_INTERVAL_S should be renamed! It's not a timeout - more of a time in
-    if (status.playing && millis() > lastCurrentTimeCheck + RADIO_CURRENT_TIME_CHECK_INTERVAL_S * 1000) {
+    if (status.playing && millis() > lastCurrentTimeCheck + config->streamLossDetectionIntervalS * 1000) {
       lastCurrentTimeCheck = millis();
       uint32_t currentTime = audio->getAudioCurrentTime();
 
-      // Give the stream RADIO_CURRENT_TIME_CHECK_INTERVAL_S * 2 to get started (otherwise this may check too soon and start a loop of reconnects)
-      if (currentTime > RADIO_CURRENT_TIME_CHECK_INTERVAL_S * 2 && currentTime - lastCurrentTime < RADIO_CURRENT_TIME_CHECK_INTERVAL_S * 0.9) {
+      // Give the stream config->streamLossDetectionIntervalS * 2 to get started (otherwise this may check too soon and start a loop of reconnects)
+      if (currentTime > config->streamLossDetectionIntervalS * 2 && currentTime - lastCurrentTime < config->streamLossDetectionIntervalS * 0.9) {
         status.playing = false;
         lastCurrentTime = currentTime;
         if (debugMode) {
--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -27,7 +27,7 @@ States:
   - Red                 LED_STATUS_ERROR                error
   - Red (blinking)      LED_STATUS_ERROR_BLINKING       error (blinking)
 
-ESP32-audioI2S Examples/Docs/Source (2.0.0, testing 3.0.0) (Installed manually):
+ESP32-audioI2S Examples/Docs/Source (testing 3.0.0) (Installed manually):
   - https://github.com/schreibfaul1/ESP32-audioI2S/blob/master/src/Audio.h
   - https://github.com/schreibfaul1/ESP32-audioI2S/blob/master/src/Audio.cpp
   - https://github.com/schreibfaul1/ESP32-audioI2S/blob/master/examples/Simple_WiFi_Radio/Simple_WiFi_Radio.ino
@@ -41,10 +41,8 @@ ArduinoJSON (6.21.2)
 TODO:
   - Pull settings from remote server
   - TODO ESP32-audioI2S 3.0.0 has lostStreamDetection!!!
-
-  Audio/DAC
-  - Make sure it's using PSRAM, document how this is done
-
+    - This does not exist in 3.0.0 - was the name changed?
+    
 */
 #define FIRMWARE_VERSION "v0.0"
 
@@ -52,29 +50,22 @@ TODO:
 #include "Audio.h"
 #include "Radio.h"
 
-#define PIN_CHANNEL_POT 2
-#define PIN_VOLUME_POT 8
-#define PIN_DAC_SD_MODE 11
-#define PIN_I2S_DOUT 12
-#define PIN_I2S_BCLK 13
-#define PIN_I2S_LRC 14
-// TODO: LED Status settings should be passed to the Radio, and then on to the LEDStatus
-
-#define VOLUME_MIN 0
-#define VOLUME_MAX 21
-
 WiFiManager wifiManager;
 Audio audio;
+RadioConfig radioConfig;
 
+Radio radio(&radioConfig, &wifiManager, &audio);
 
-// make a config struct, with default values. radio(radioConfig, &wifiManager, &audio)
-Radio radio(PIN_CHANNEL_POT, PIN_VOLUME_POT, VOLUME_MIN, VOLUME_MAX, &wifiManager, &audio);
+// Callbacks which need the radio instance
+void audio_info(const char *info) {
+  // Triggered once a feed is played.
+  if (radio.debugMode) {
+    Serial.print("info:");
+    Serial.println(info);
+  }
+}
 
-
-
-
-// TODO these should at least fire a method, not handle it here. 
-void configModeCallback(WiFiManager *myWiFiManager) {
+void wifiManagerSetupCallback(WiFiManager *myWiFiManager) {
   radio.ledStatus.setStatus(LED_STATUS_WARNING_BLINKING);
   if (radio.debugMode) {
     Serial.println("Entered config mode");
@@ -84,59 +75,16 @@ void configModeCallback(WiFiManager *myWiFiManager) {
   }
 }
 
-void audio_info(const char *info) {
-  // Triggered once a feed is played.
-  if (radio.debugMode) {
-    Serial.print("info:");
-    Serial.println(info);
-  }
-}
-
-
-
 
 void setup() {
 
-  // Consider moving this to the Radio object
-  pinMode(PIN_DAC_SD_MODE, OUTPUT);
-
-  // Turn DAC on
-  digitalWrite(PIN_DAC_SD_MODE, HIGH);
-
   // radio.debugMode = true;
-
-  // This should be run first to make sure that debug mode is set right away.
+  
+  wifiManager.setAPCallback(wifiManagerSetupCallback);
   radio.init();
 
-  // WiFiManager
-  WiFi.mode(WIFI_STA);
-  wifiManager.setDebugOutput(radio.debugMode);
-  wifiManager.setAPCallback(configModeCallback);
-  bool res;
-  res = wifiManager.autoConnect("Radio Setup");
-  if (!res) {
-    if (radio.debugMode) Serial.println("Failed to connect or hit timeout");
-    // This is a hard stop
-    radio.ledStatus.setStatus(LED_STATUS_ERROR_BLINKING);
-    delay(5000);
-    ESP.restart();
-  }
-
-  audio.setPinout(PIN_I2S_BCLK, PIN_I2S_LRC, PIN_I2S_DOUT);
-  audio.setVolume(0);
-
-  // Turn lights off if it connects to the remote list server, or does not need to.
-  radio.ledStatus.setStatus(LED_STATUS_LIGHTS_OFF);
-
-  // Get stations from config server
-  bool error = radio.getConfigFromRemote();
-  if (error) {
-    radio.ledStatus.setStatus(LED_STATUS_WARNING_BLINKING);
-    delay(5000);
-  }
 }
 
 void loop() {
-  audio.loop();
   radio.loop();
 }
