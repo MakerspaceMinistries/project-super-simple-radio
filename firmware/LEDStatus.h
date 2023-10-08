4681377cda8c896201020af39b8d740ff73702ef
jake <jaketeater82@gmail.com>
2023-10-08 00:00:05 -0400

    Completely rewrote the LEDStatus object, simplifying it. It is not implemented

--- firmware/LEDStatus.h
+++ firmware/LEDStatus.h
@@ -1,364 +1,241 @@
-// https://espressif-docs.readthedocs-hosted.com/projects/arduino-esp32/en/latest/api/timer.html
-
 /*
-Status Options:
-  - Special (Blocking)  LED_STATUS_FADE_SUCCESS_TO_INFO
-  - Lights Off          LED_STATUS_LIGHTS_OFF           
-  - Green               LED_STATUS_SUCCESS              success
-  - Green (blinking)    LED_STATUS_SUCCESS_BLINKING     success (blinking)
-  - Blue                LED_STATUS_INFO                 info
-  - Blue (blinking)     LED_STATUS_INFO_BLINKING        info (blinking)
-  - Yellow              LED_STATUS_WARNING              warning
-  - Yellow              LED_STATUS_WARNING_BLINKING     warning (blinking)
-  - Red                 LED_STATUS_ERROR                error
-  - Red (blinking)      LED_STATUS_ERROR_BLINKING       error (blinking)
 
-Status Codes:
+See the public methods for documentation on how each works
 
-  Status codes (in the form of levels and codes) can be sent instead of LED Statuses. These codes are then interpreted into LED Statuses, and the status code is also stored.
+-100     Unset (Idle)
+    0- 49 Info
+  50- 99 Info (Blinking)
+  100-149 Success
+  150-199 Success (Blinking)
+  200-249 Warning
+  250-299 Warning (Blinking)
+  300-349 Error
+  350-399 Error (Blinking)
 
-  Each status status code is stored individually by level, but only one level is displayed.
-
-  Codes are hierarchical in when/how they are displayed, in that an error code will take priority in being displayed over a success code. Likewise, a success code will have priority over an info code.
-
-  If a higher priority status code is cleared, a previously set lower priority status code will then become active.
-
-  -  -1     Unset (Idle)
-  -   0- 49 Info
-  -  50- 99 Info (Blinking)
-  - 100-149 Success
-  - 150-199 Success (Blinking)
-  - 200-249 Warning
-  - 250-299 Warning (Blinking)
-  - 300-349 Error
-  - 350-399 Error (Blinking)
-
-TODO:
-
-This in the switch should be made into a function:
-
-  writeRGB(mLedOff, mLedOn, mLedOff);
-  setRGBActive(0, 1, 0);
-  timerEnable();
+Docs on hardware timers
+https://espressif-docs.readthedocs-hosted.com/projects/arduino-esp32/en/latest/api/timer.html
 
 */
 
+
 #include "esp_system.h"
 
-#define LED_STATUS_FADE_SUCCESS_TO_INFO_ITERATIONS 1000
-#define LED_STATUS_FADE_SUCCESS_TO_INFO 0
-#define LED_STATUS_LIGHTS_OFF 1
-#define LED_STATUS_SUCCESS 2
-#define LED_STATUS_SUCCESS_BLINKING 3
-#define LED_STATUS_INFO 4
-#define LED_STATUS_INFO_BLINKING 5
-#define LED_STATUS_WARNING 6
-#define LED_STATUS_WARNING_BLINKING 7
-#define LED_STATUS_ERROR 8
-#define LED_STATUS_ERROR_BLINKING 9
+#define LED_STATUS_BLINKING_THRESHOLD 49
+#define LED_STATUS_UNSET -999
+#define LED_STATUS_IDLE_STATUS_CODE -100
+#define LED_STATUS_LEVEL_000_BLUE_INFO 0
+#define LED_STATUS_LEVEL_100_GREEN_SUCCESS 100
+#define LED_STATUS_LEVEL_200_YELLOW_WARNING 200
+#define LED_STATUS_LEVEL_300_RED_ERROR 300
+#define LED_STATUS_MAX_CODE 400
 
-#define LED_STATUS_DEFAULT_PIN_LED_RED 4
-#define LED_STATUS_DEFAULT_PIN_LED_GREEN 5
-#define LED_STATUS_DEFAULT_PIN_LED_BLUE 6
-#define LED_STATUS_DEFAULT_LED_ON LOW
-#define LED_STATUS_DEFAULT_LED_OFF HIGH
+int g_led_status_rgb_pins[3] = { 0, 0, 0 };
+bool g_led_status_rgb_is_active[3] = { false, false, false };
 
-#define LED_STATUS_DEFAULT_TIMER_NUMBER 0
-#define LED_STATUS_DEFAULT_TIMER_DIVIDER 80
-#define LED_STATUS_DEFAULT_TIMER_ALARM_VALUE 600000
-
-#define LED_STATUS_UNSET_STATUS_CODE -1
-
-#define LED_STATUS_HIGHEST_LEVEL 3
-
-#define LED_STATUS_300_ERROR_LEVEL 3
-#define LED_STATUS_200_WARNING_LEVEL 2
-#define LED_STATUS_100_SUCCESS_LEVEL 1
-#define LED_STATUS_000_INFO_LEVEL 0
-
-#define LED_STATUS_INFO_LIGHTS_OFF_CODE 0
-
-// The callback and variables required by the callback are global for simplicity's sake.
-// This explains how to get around that: https://arduino.stackexchange.com/a/89176
-int gLedStatusRGBPins[3]{ 0, 0, 0 };
-int gLedStatusRGBActive[3] = { 0, 0, 0 };
-void timerCallback() {
-  // DO NOT change any variables here (that are from outside this scope)
-  for (int i; i < 3; i++) {
-    if (gLedStatusRGBActive[i] == 1) {
-      int state = digitalRead(gLedStatusRGBPins[i]);
-      digitalWrite(gLedStatusRGBPins[i], !state);
+void led_status_timer_callback() {
+  for (int i = 0; i < 3; i++) {
+    if (g_led_status_rgb_is_active[i]) {
+      int state = digitalRead(g_led_status_rgb_pins[i]);
+      digitalWrite(g_led_status_rgb_pins[i], !state);
     }
   }
 }
 
-class LEDStatus {
-  int mLedOff;
-  int mLedOn;
-  int mTimerNumber;
-  int mTimerDivider;
-  int mTimerAlarmValue;
-  int mStatusCodes[4] = { LED_STATUS_UNSET_STATUS_CODE, LED_STATUS_UNSET_STATUS_CODE, LED_STATUS_UNSET_STATUS_CODE, LED_STATUS_UNSET_STATUS_CODE };
-  int mActiveStatusCode = 0;
-  hw_timer_t *mTimerBlink = NULL;
-  void setRGBActive(int r, int g, int b);
-  void timerEnable();
-  void timerDisable();
-  void writeRGB(int r, int g, int b);
-  void computeActiveStatusCode(int blockingDelay);
-  int getIndexOfActiveStatus();
-  int levelCode2StatusCode(int level, int code);
-  bool isBlinkingCode(int code);
-
-public:
-  LEDStatus();
-  bool mDebugMode;
-  void configure(int rPin, int gPin, int bPin, int ledOff, int ledOn, int timerNumber, int timerDivider, int timerAlarmValue);
-  void setRGB(int r, int g, int b);
-  void setStatus(int status);
-  void setStatusCode(int statusCode, int clearToLevel, int blockingDelay);
-  void setStatusCode(int level, int code, int clearToLevel, int blockingDelay);
-  void clearStatusCode(int statusCode);
-  void clearStatusLevel(int level);
-  void clearAllStatusCodesFromZeroTo(int level);
-  int getActiveStatusCode();
-  void init(bool debugMode);
+struct LEDStatusConfig {
+  int rgb_pins[3] = { 4, 5, 6 };
+  int led_on = LOW;
+  int led_off = HIGH;
+  int timer_number = 0;
+  int timer_divider = 80;
+  int timer_alarm_value = 600000;
 };
 
-LEDStatus::LEDStatus() {
-  // This should be refactored along with the configure method. Make a LEDStatusConfig struct
-  mLedOn = LED_STATUS_DEFAULT_LED_ON;
-  mLedOff = LED_STATUS_DEFAULT_LED_OFF;
-  mTimerNumber = LED_STATUS_DEFAULT_TIMER_NUMBER;
-  mTimerDivider = LED_STATUS_DEFAULT_TIMER_DIVIDER;
-  mTimerAlarmValue = LED_STATUS_DEFAULT_TIMER_ALARM_VALUE;
-  gLedStatusRGBPins[0] = LED_STATUS_DEFAULT_PIN_LED_RED;
-  gLedStatusRGBPins[1] = LED_STATUS_DEFAULT_PIN_LED_GREEN;
-  gLedStatusRGBPins[2] = LED_STATUS_DEFAULT_PIN_LED_BLUE;
+class LEDStatus {
+public:
+  LEDStatus(LEDStatusConfig *config);
+  int get_status();
+  void init(bool debug);
+  void set_status(int status, int force_to_status = LED_STATUS_UNSET);
+  void set_debug(bool debug);
+  void clear_status(int status);
+  void clear_all_statuses();
+
+private:
+  LEDStatusConfig *m_config_;
+  int m_status_ = LED_STATUS_IDLE_STATUS_CODE;
+  int m_major_status_ = -100;
+  int m_minor_status_ = 0;
+  bool m_debug_ = false;
+  hw_timer_t *m_hardware_timer_ = NULL;
+
+  bool is_blinking_status_();
+  void write_rgb_(bool rgb[3]);
+  void debug_output(const char *message, int value);
+  void status_to_rgb_();
+  void hardware_timer_enable_();
+  void hardware_timer_disable_();
+  void set_status_(int status);
+};
+
+LEDStatus::LEDStatus(LEDStatusConfig *config) {
+  m_config_ = config;
 }
 
-void LEDStatus::configure(int rPin, int gPin, int bPin, int ledOff = LED_STATUS_DEFAULT_LED_OFF, int ledOn = LED_STATUS_DEFAULT_LED_ON, int timerNumber = LED_STATUS_DEFAULT_TIMER_NUMBER, int timerDivider = LED_STATUS_DEFAULT_TIMER_DIVIDER, int timerAlarmValue = LED_STATUS_DEFAULT_TIMER_ALARM_VALUE) {
-  mLedOn = ledOn;
-  mLedOff = ledOff;
-  mTimerNumber = timerNumber;
-  mTimerDivider = timerDivider;
-  mTimerAlarmValue = timerAlarmValue;
-  gLedStatusRGBPins[0] = rPin;
-  gLedStatusRGBPins[1] = gPin;
-  gLedStatusRGBPins[2] = bPin;
+/**
+ * Initializes the LEDStatus instance.
+ * 
+ * This function must be called before using other methods in this class. It sets up the pins for the LEDs and initializes the timer.
+ *
+ * @param debug Boolean indicating if debug mode is on. If true, debug messages will be sent to the Serial output.
+ */
+void LEDStatus::init(bool debug) {
+  m_debug_ = debug;
+
+  for (int i = 0; i < 3; i++) {
+    g_led_status_rgb_pins[i] = m_config_->rgb_pins[i];
+  }
+
+  pinMode(g_led_status_rgb_pins[0], OUTPUT);
+  pinMode(g_led_status_rgb_pins[1], OUTPUT);
+  pinMode(g_led_status_rgb_pins[2], OUTPUT);
+  bool rgb[3] = { false, false, false };
+  write_rgb_(rgb);
+
+  m_hardware_timer_ = timerBegin(m_config_->timer_number, m_config_->timer_divider, true);
+  timerAttachInterrupt(m_hardware_timer_, led_status_timer_callback, true);
+  timerAlarmWrite(m_hardware_timer_, m_config_->timer_alarm_value, true);
 }
 
-void LEDStatus::setRGBActive(int r, int g, int b) {
-  gLedStatusRGBActive[0] = r;
-  gLedStatusRGBActive[1] = g;
-  gLedStatusRGBActive[2] = b;
+/**
+ * Returns the current status code.
+ * 
+ * @return The current status code set in the LEDStatus instance.
+ */
+int LEDStatus::get_status() {
+  return m_status_;
 }
 
-void LEDStatus::timerEnable() {
-  timerAlarmEnable(mTimerBlink);
-}
+/**
+ * Sets the status code.
+ * 
+ * This function will update the LED's color and blink state based on the status code provided. If the current status is more important than the new status, the new status won't overwrite the current status unless forced.
+ *
+ * @param status The new status code to set.
+ * @param force_to_status Optional parameter. If provided and is greater than the current status, this status will be set even if it's considered less important than the current status.
+ */
+void LEDStatus::set_status(int status, int force_to_status) {
 
-void LEDStatus::timerDisable() {
-  timerAlarmDisable(mTimerBlink);
-}
+  force_to_status = (force_to_status == LED_STATUS_UNSET) ? status : force_to_status;
 
-void LEDStatus::init(bool debugMode = false) {
-  mDebugMode = debugMode;
-  pinMode(gLedStatusRGBPins[0], OUTPUT);
-  pinMode(gLedStatusRGBPins[1], OUTPUT);
-  pinMode(gLedStatusRGBPins[2], OUTPUT);
-  writeRGB(mLedOff, mLedOff, mLedOff);
-  mTimerBlink = timerBegin(mTimerNumber, mTimerDivider, true);
-  timerAttachInterrupt(mTimerBlink, timerCallback, true);
-  timerAlarmWrite(mTimerBlink, mTimerAlarmValue, true);
-}
+  if (status == m_status_) return;
 
-void LEDStatus::setStatus(int status) {
-  writeRGB(mLedOff, mLedOff, mLedOff);
-  timerDisable();
-  switch (status) {
-    case LED_STATUS_FADE_SUCCESS_TO_INFO:
-      writeRGB(mLedOff, mLedOn, mLedOff);
-      delay(250);
-      for (int i = 0; i < LED_STATUS_FADE_SUCCESS_TO_INFO_ITERATIONS; i++) {
-        writeRGB(mLedOff, mLedOn, mLedOff);
-        delayMicroseconds(LED_STATUS_FADE_SUCCESS_TO_INFO_ITERATIONS - i);
-        writeRGB(mLedOff, mLedOff, mLedOn);
-        delayMicroseconds(i);
-      }
-      break;
-    case LED_STATUS_LIGHTS_OFF:
-      // This is taken care of above.
-      break;
-    case LED_STATUS_SUCCESS:
-      writeRGB(mLedOff, mLedOn, mLedOff);
-      break;
-    case LED_STATUS_SUCCESS_BLINKING:
-      writeRGB(mLedOff, mLedOn, mLedOff);
-      setRGBActive(0, 1, 0);
-      timerEnable();
-      break;
-    case LED_STATUS_INFO:
-      writeRGB(mLedOff, mLedOff, mLedOn);
-      break;
-    case LED_STATUS_INFO_BLINKING:
-      writeRGB(mLedOff, mLedOff, mLedOn);
-      setRGBActive(0, 0, 1);
-      timerEnable();
-      break;
-    case LED_STATUS_WARNING:
-      writeRGB(mLedOn, mLedOn, mLedOff);
-      break;
-    case LED_STATUS_WARNING_BLINKING:
-      writeRGB(mLedOn, mLedOn, mLedOff);
-      setRGBActive(1, 1, 0);
-      timerEnable();
-      break;
-    case LED_STATUS_ERROR:
-      writeRGB(mLedOn, mLedOff, mLedOff);
-      break;
-    case LED_STATUS_ERROR_BLINKING:
-      writeRGB(mLedOn, mLedOff, mLedOff);
-      setRGBActive(1, 0, 0);
-      timerEnable();
-      break;
+  if (m_status_ <= force_to_status) {
+    debug_output("New status: ", status);
+    set_status_(status);
+    status_to_rgb_();
+  } else {
+    debug_output("Unable to overwrite current status with: ", status);
   }
 }
 
-void LEDStatus::writeRGB(int r, int g, int b) {
-  digitalWrite(gLedStatusRGBPins[0], r);
-  digitalWrite(gLedStatusRGBPins[1], g);
-  digitalWrite(gLedStatusRGBPins[2], b);
-}
-
-int LEDStatus::getIndexOfActiveStatus() {
-  int idx = 0;
-
-  // No need to compare the 0th to itself, start at 1
-  for (int i = 1; i < 4; i++) {
-
-    // If the value is unset, then no need to do anything, otherwise compute code based using the index and compare
-    if (mStatusCodes[i] != LED_STATUS_UNSET_STATUS_CODE && levelCode2StatusCode(i, mStatusCodes[i]) > levelCode2StatusCode(idx, mStatusCodes[idx])) {
-      idx = i;
-    }
-  }
-  return idx;
-}
-
-void LEDStatus::setStatusCode(int statusCode, int clearToLevel = -1, int blockingDelay = 0) {
-  int level = statusCode / 100;
-  int code = statusCode - level * 100;
-  setStatusCode(level, code, clearToLevel, blockingDelay);
-}
-
-void LEDStatus::setStatusCode(int level, int code, int clearToLevel, int blockingDelay) {
-  /*
-  Using blockingDelay sparingly, as it blocks ;)
-  */
-
-  if (levelCode2StatusCode(level, code) == mActiveStatusCode) { return; }
-  
-  clearAllStatusCodesFromZeroTo(clearToLevel);
-
-  if (mDebugMode) {
-    Serial.print("Request to set Status Code to: ");
-    Serial.println(levelCode2StatusCode(level, code));
-    Serial.print("Active Status Code: ");
-    Serial.println(mActiveStatusCode);
-  }
-
-  mStatusCodes[level] = code;
-  computeActiveStatusCode(blockingDelay);
-
-  if (mDebugMode) {
-    Serial.print("New Active Status Code: ");
-    Serial.println(mActiveStatusCode);
+/**
+ * Clears a specific status.
+ * 
+ * If the current status matches the provided status code, the status will be cleared and set to the unset (idle) state.
+ *
+ * @param status The status code to clear.
+ */
+void LEDStatus::clear_status(int status) {
+  if (m_status_ == status) {
+    debug_output("Clearing status: ", status);
+    set_status(LED_STATUS_IDLE_STATUS_CODE, status);
+  } else {
+    debug_output("Attempting to clear status that is not active: ", status);
   }
 }
 
-void LEDStatus::clearStatusCode(int statusCode) {
+/**
+ * Clears all statuses.
+ * 
+ * Resets the status code to the unset (idle) state regardless of the current status.
+ */
+void LEDStatus::clear_all_statuses() {
+  debug_output("Resetting status to: ", LED_STATUS_IDLE_STATUS_CODE);
+  set_status(LED_STATUS_IDLE_STATUS_CODE, LED_STATUS_MAX_CODE);
+}
 
-  // TODO: refactor this - it's duplicated
-  int level = statusCode / 100;
-  int code = statusCode - level * 100;
+/**
+ * Enables or disables debug mode.
+ * 
+ * In debug mode, messages are sent to the Serial output to help diagnose issues and monitor the behavior of the LEDStatus instance.
+ *
+ * @param debug Boolean indicating if debug mode should be enabled (true) or disabled (false).
+ */
+void LEDStatus::set_debug(bool debug) {
+  m_debug_ = debug;
+}
 
-  // TODO: refactor this - mStatusCodes[level] - it's duplicated and should be a getter that takes a level. The comparator could be refactored as well?
-  if (code == mStatusCodes[level]) {
-    int level = statusCode / 100;
-    setStatusCode(level, LED_STATUS_UNSET_STATUS_CODE, -1, 0);
+void LEDStatus::set_status_(int status) {
+  m_status_ = status;
+  m_major_status_ = (status / 100) * 100;
+  m_minor_status_ = status - m_major_status_;
+}
+
+void LEDStatus::write_rgb_(bool rgb[3]) {
+  for (int i = 0; i < 3; i++) {
+    g_led_status_rgb_is_active[i] = rgb[i];
+    int state = (rgb[i]) ? m_config_->led_on : m_config_->led_off;
+    digitalWrite(g_led_status_rgb_pins[i], state);
   }
-  return;
 }
 
-void LEDStatus::clearStatusLevel(int level) {
-  return setStatusCode(level, LED_STATUS_UNSET_STATUS_CODE, -1, 0);
-}
-
-void LEDStatus::clearAllStatusCodesFromZeroTo(int level) {
-
-  // Just in case a status code is passed in stead of a level, this will prevent overwriting data.
-  if (level > LED_STATUS_HIGHEST_LEVEL) {
-    level = level / 100;
-  }
-
-  for (int i = level; i >= 0; i--) {
-    mStatusCodes[i] = LED_STATUS_UNSET_STATUS_CODE;
+void LEDStatus::debug_output(const char *message, int state) {
+  if (m_debug_) {
+    Serial.print(message);
+    Serial.println(state);
   }
 }
 
-int LEDStatus::getActiveStatusCode() {
-  return mActiveStatusCode;
+bool LEDStatus::is_blinking_status_() {
+  return (m_minor_status_ > LED_STATUS_BLINKING_THRESHOLD);
 }
 
-int LEDStatus::levelCode2StatusCode(int level, int code) {
-  return level * 100 + code;
-}
+void LEDStatus::status_to_rgb_() {
+  hardware_timer_disable_();
 
-bool LEDStatus::isBlinkingCode(int code) {
-  return (code > 49);
-}
+  bool rgb[3] = { false, false, false };
 
-void LEDStatus::computeActiveStatusCode(int blockingDelay = 0) {
-  int level = getIndexOfActiveStatus();
-  int code = mStatusCodes[level];
-  int statusCode = levelCode2StatusCode(level, code);
-  mActiveStatusCode = statusCode;
-
-  bool blinking = isBlinkingCode(code);
-  switch (level) {
-    case 0:  // Info
-      if (code == LED_STATUS_INFO_LIGHTS_OFF_CODE) {
-        setStatus(LED_STATUS_LIGHTS_OFF);
-      } else if (blinking) {
-        setStatus(LED_STATUS_INFO_BLINKING);
-      } else {
-        setStatus(LED_STATUS_INFO);
-      }
+  switch (m_major_status_) {
+    case LED_STATUS_IDLE_STATUS_CODE:
+      // Do nothing, leaving rgb at it's default value of all LEDs being off.
       break;
-    case 1:  // Success
-      if (blinking) {
-        setStatus(LED_STATUS_SUCCESS_BLINKING);
-      } else {
-        setStatus(LED_STATUS_SUCCESS);
-      }
+    case LED_STATUS_LEVEL_000_BLUE_INFO:
+      rgb[2] = true;
       break;
-    case 2:  // Warning
-      if (blinking) {
-        setStatus(LED_STATUS_WARNING_BLINKING);
-      } else {
-        setStatus(LED_STATUS_WARNING);
-      }
+    case LED_STATUS_LEVEL_100_GREEN_SUCCESS:
+      rgb[1] = true;
       break;
-    case 3:  // Error
-      if (blinking) {
-        setStatus(LED_STATUS_ERROR_BLINKING);
-      } else {
-        setStatus(LED_STATUS_ERROR);
-      }
+    case LED_STATUS_LEVEL_200_YELLOW_WARNING:
+      rgb[0] = true;
+      rgb[1] = true;
+      break;
+    case LED_STATUS_LEVEL_300_RED_ERROR:
+      rgb[0] = true;
       break;
   }
 
-  if (blockingDelay > 0) {
-    delay(blockingDelay);
+  write_rgb_(rgb);
+
+  if (is_blinking_status_()) {
+    hardware_timer_enable_();
   }
 }
+
+void LEDStatus::hardware_timer_enable_() {
+  timerAlarmEnable(m_hardware_timer_);
+}
+
+void LEDStatus::hardware_timer_disable_() {
+  timerAlarmDisable(m_hardware_timer_);
+}
+
