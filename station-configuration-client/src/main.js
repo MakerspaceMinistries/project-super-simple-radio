c2c99f4019fdc857d97a4d5aefd147b9eeb36456
Jake - Laptop <jaketeater82@gmail.com>
2024-02-03 14:49:04 -0500

    Init work on webclient

--- station-configuration-client/src/main.js
+++ station-configuration-client/src/main.js
@@ -7,6 +7,7 @@ import App from './App.vue'
 import router from './router'
 
 import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
@@ -14,6 +15,9 @@ import * as directives from 'vuetify/directives'
 const vuetify = createVuetify({
   components,
   directives,
+  icons: {
+    defaultSet: 'mdi' // This is already the default value - only for display purposes
+  }
 })
 
 const app = createApp(App)
---
+++ station-configuration-client/src/settings.js
@@ -0,0 +1,4 @@
+const apiHost = 'http://config.acc-radio.raiotech.com'
+const supportContact = 'Add Support Contact To settings.js'
+
+export { apiHost, supportContact }
