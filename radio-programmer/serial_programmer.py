eb9cf0e087a443afe534a9c018ec7f5912d82fdd
Jake - Laptop <jaketeater82@gmail.com>
2024-02-05 22:13:36 -0500

    refactored so that there's only one serial connection, handled serial more stabily

--- radio-programmer/serial_programmer.py
+++ radio-programmer/serial_programmer.py
@@ -1,14 +1,10 @@
-'''
-
-This needs refactored and a class object should probably be used.
+# This needs refactored and a class object should probably be used.
 
 
-Examples:
+# Examples:
 
-Auto detect the serial port and write firmware.
-python .\serial_programmer.py write_firmware -d --firmware-version v1.0.0-beta.2
-
-'''
+# Auto detect the serial port and write firmware.
+# python .\serial_programmer.py write_firmware -d --firmware-version v1.0.0-beta.2
 
 import serial
 import json
@@ -35,27 +31,25 @@ parser.add_argument('-l', '--label', type=str, help='Label for the radio.')
 parser.add_argument('-n', '--network', type=int, help='network_id of the network to which the radio belongs.')
 parser.add_argument('-p', '--password', type=str, help='Password for the configuration server.')
 parser.add_argument('-r', '--remote-config-background-retrieval-interval', default=43200000, type=int, help='remote_config_background_retrieval_interval')
-parser.add_argument('-t', '--target', default='/dev/ttyACM0', type=str, help='Target device to write to. Default: /dev/ACM0')
+parser.add_argument('-s', '--default-station', default=None, type=str, help='Default station to be written into position 0.')
+parser.add_argument('-t', '--target', default=None, type=str, help='Target device to write to. Default: /dev/ACM0')
 parser.add_argument('-u', '--user', type=str, help='User name for the configuration server.')
 parser.add_argument('-V', '--firmware-version', type=str, help='Version used when writing firmware. Ex: v1.0.0')
 parser.add_argument('-w', '--wifi', action='store_true', help='Configure the radio to connect to the default WiFi Network.')
 
 args = parser.parse_args()
 
-def read_config(port):
-    with serial.Serial(port, 115200, timeout=2) as ser:
-        while True:
-            ser.flush()
-            ser.write("{}".encode())
-            sleep(0.1)
-            data = ser.read_all().decode().strip()
-            if data:
-                print(data, flush=True)
-                break
+def read_config(ser):
+    ser.flush()
+    ser.write("{}".encode())
+    while True:
+        sleep(0.1)
+        data = ser.read_all().decode().strip()
+        if data:
+            print(data, flush=True)
+            break
 
-def create_radio(args):
-    radio_id, qr_data = detect_and_decode_qr()
-    
+def create_radio(args, radio_id):
     with requests.Session() as session:
 
         # Construct login URL
@@ -94,37 +88,45 @@ def create_radio(args):
         else:
             raise Exception("Failed to log in")
 
-def write_to_serial(port, payload):
-    # Open the serial port
-    with serial.Serial(port, 115200) as ser:
-        # Check if the payload is a dictionary
-        if isinstance(payload, dict):
-            # Convert the dictionary to a JSON string
-            payload = json.dumps(payload)
-        
-        # Write the payload to the serial port
-        # The encode() method converts the string to bytes, which is required by the write() method
-        print(f"Writing: {payload}")
-        ser.write(payload.encode())
-        ser.flush()
-        sleep(0.1)
+def write_to_serial(ser, payload):
+    # Check if the payload is a dictionary
+    if isinstance(payload, dict):
+        # Convert the dictionary to a JSON string
+        payload = json.dumps(payload)
+    
+    # Write the payload to the serial port
+    # The encode() method converts the string to bytes, which is required by the write() method
+    print(f"Writing: {payload}")
+    ser.write(payload.encode())
+    sleep(0.1)
 
-def update_radio(args):
-    radio_id, qr_data = detect_and_decode_qr()
 
-    write_key_value_to_serial(args.target, "remote_config",  True)
-    write_key_value_to_serial(args.target, "radio_id", radio_id)
-    write_key_value_to_serial(args.target, "remote_cfg_url",  f"{args.host}/api/{args.api_version}/radios/device_interface/{args.config_endpoint_version}/")
-    write_key_value_to_serial(args.target, "has_channel_pot",  args.has_channel_pot)
-    write_key_value_to_serial(args.target, "pcb_version", args.pcb_version )
-    write_key_value_to_serial(args.target, "remote_config_background_retrieval_interval", args.remote_config_background_retrieval_interval )
+def update_radio(ser, args, radio_id):
+    read_config(ser)
+    write_key_value_to_serial(ser, "remote_config",  True)
+    write_key_value_to_serial(ser, "radio_id", radio_id)
+    write_key_value_to_serial(ser, "remote_cfg_url",  f"{args.host}/api/{args.api_version}/radios/device_interface/{args.config_endpoint_version}/")
+    write_key_value_to_serial(ser, "has_channel_pot",  args.has_channel_pot)
+    write_key_value_to_serial(ser, "pcb_version", args.pcb_version )
+    write_key_value_to_serial(ser, "remote_config_background_retrieval_interval", args.remote_config_background_retrieval_interval )
+    if args.default_station:
+        write_key_value_to_serial(ser, "stn_1_url", args.default_station)
+    read_config(ser)
+
+    # This will restart the ESP, so run last.
     if args.wifi:
         wifi_creds = {"ssid": "radio-setup", "pass": "supersimpleradio"}
-        write_to_serial(args.target, wifi_creds)
+        write_to_serial(ser, wifi_creds)
 
-def write_key_value_to_serial(port, key, value):
+def write_key_value_to_serial(ser, key, value):
     kv = {key:value}
-    write_to_serial(port, kv)
+    write_to_serial(ser, kv)
+    while True:
+        sleep(0.1)
+        data = ser.read_all().decode().strip()
+        if data:
+            break
+    sleep(0.1)
 
 def load_settings_file(args):
     with open(args.file, 'r') as file:
@@ -135,36 +137,38 @@ def load_settings_file(args):
 if __name__ == '__main__':
 
     # TODO add action to generate QR codes
+    if args.file:
+        load_settings_file(args)
 
     if args.auto_detect_serial_port:
         port = serial_ports()[0]
     else:
         port = args.target
 
-    if args.file:
-        load_settings_file(args)
+    # THERE SHOULD ONLY BE ONE SERIAL CONNECTION SO IT DOESN'T REBOOT
+    # It will reboot entering/leaving the connection.
+    with serial.Serial(port, 115200) as ser:
 
-    if args.action in ['create', 'update']:
-         # This will print out the config. It should also verify the config.
-        read_config(port)
-        radio_id, qr_data = detect_and_decode_qr()
+        if args.action in ['create', 'update']:
+            # This will print out the config. It should also verify the config.
+            radio_id, qr_data = detect_and_decode_qr()
 
-    if args.action == 'create':
-        radio_id = create_radio(args)
-        print(f"New radio added to database: {radio_id}")
-        update_radio(args)
-        print("Radio updated.")
-    
-    elif args.action == 'update':
-        update_radio(args)
+        if args.action == 'create':
+            radio_id = create_radio(args, radio_id)
+            print(f"New radio added to database: {radio_id}")
+            update_radio(ser, args, radio_id)
+            print("Radio updated.")
+        
+        elif args.action == 'update':
+            update_radio(ser, args, radio_id)
 
-    elif args.action == 'write_firmware':
-        print('If writing over UART, be sure to press the boot to program button', flush=True)
-        esptool.main(['--chip', 'esp32s3', '--port', port, '--baud', '921600',  '--before', 
-                      'default_reset', '--after', 'hard_reset', 'write_flash',  '-z', 
-                      '--flash_mode', 'dio', '--flash_freq', '80m', '--flash_size', '4MB', 
-                      '0x0', f'./{args.firmware_version}/firmware.ino.bootloader.bin', '0x8000', 
-                      f'./{args.firmware_version}/firmware.ino.partitions.bin', '0xe000', 
-                      './libs/esp32s3-2.0.14.boot_app0.bin', '0x10000', 
-                      f'./{args.firmware_version}/firmware.ino.bin'])
+        elif args.action == 'write_firmware':
+            print('If writing over UART, be sure to press the boot to program button', flush=True)
+            esptool.main(['--chip', 'esp32s3', '--port', port, '--baud', '921600',  '--before', 
+                        'default_reset', '--after', 'hard_reset', 'write_flash',  '-z', 
+                        '--flash_mode', 'dio', '--flash_freq', '80m', '--flash_size', '4MB', 
+                        '0x0', f'./{args.firmware_version}/firmware.ino.bootloader.bin', '0x8000', 
+                        f'./{args.firmware_version}/firmware.ino.partitions.bin', '0xe000', 
+                        './libs/esp32s3-2.0.14.boot_app0.bin', '0x10000', 
+                        f'./{args.firmware_version}/firmware.ino.bin'])
 
